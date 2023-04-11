f0d319fb5f32985fd0b7645aedfb63cada9e16a1
davidhanenko <d.hanenko24@gmail.com>
2023-04-11 12:02:36 -0400

    init

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>HOME</h1>
+      <div>
+        {!!user ? (
+          <>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                width={50}
+                height={50}
+              />
+              <div>{user.email}</div>
+            </div>
+            <Link href='/api/auth/logout'>Logout</Link>
+          </>
+        ) : (
+          <Link href='/api/auth/login'>Login</Link>
+        )}
+      </div>
+    </div>
+  );
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,3 @@
+export default function TokenTopup() {
+  return <h1>TOPUP</h1>;
+}
